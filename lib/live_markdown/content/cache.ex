17f6473495208258fd5e235798f7f156e01b5cb7
alfredbaudisch <alfred@alfredbaudisch.com>
2021-08-30 16:52:16 +0200

    Improved caching with indexing by path and by slug

--- lib/live_markdown/content/cache.ex
+++ lib/live_markdown/content/cache.ex
@@ -1,15 +1,30 @@
 defmodule LiveMarkdown.Content.Cache do
   require Logger
-  @cache_name Application.compile_env(:live_markdown, [LiveMarkdown.Content, :cache_name])
+  alias LiveMarkdown.Content
 
-  def save(key, value) do
-    Logger.info("[Content.Cache] saved #{key}, #{inspect(value)}")
+  @cache_name Application.compile_env(:live_markdown, [LiveMarkdown.Content, :cache_name])
+  @index_cache_name Application.compile_env(:live_markdown, [
+                      LiveMarkdown.Content,
+                      :index_cache_name
+                    ])
+
+  def save(%Content{slug: slug, file_path: path} = value) do
+    key = get_key(slug)
+    Logger.info("[Content.Cache] saved #{inspect(key)}, #{inspect(value)}")
     ConCache.put(@cache_name, key, value)
+    ConCache.put(@index_cache_name, get_path_key(path), key)
+  end
+
+  def get_by_slug(slug) do
+    ConCache.get(@cache_name, get_key(slug))
   end
 
   def get_all do
     ConCache.ets(@cache_name)
     |> :ets.tab2list()
-    |> Enum.map(fn {_key, value} -> value end)
+    |> Enum.map(fn {_, value} -> value end)
   end
+
+  defp get_key(slug), do: {:slug, slug}
+  defp get_path_key(path), do: {:path, path}
 end
