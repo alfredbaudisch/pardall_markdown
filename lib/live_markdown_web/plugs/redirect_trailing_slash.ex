f987bf5fcfb147df16153ad40a9780cb97cdf90a
alfredbaudisch <alfred@alfredbaudisch.com>
2021-09-06 21:57:48 +0200

    Dedup

--- lib/live_markdown_web/plugs/redirect_trailing_slash.ex
+++ lib/live_markdown_web/plugs/redirect_trailing_slash.ex
@@ -4,9 +4,7 @@ defmodule LiveMarkdownWeb.PlugRedirectTrailingSlash do
   def call(%{request_path: request_path} = conn, :without) when request_path != "/" do
     if String.ends_with?(request_path, "/") do
       conn
-      |> Plug.Conn.put_status(301)
-      |> Phoenix.Controller.redirect(to: String.slice(request_path, 0..-2))
-      |> Plug.Conn.halt()
+      |> redirect(String.slice(request_path, 0..-2))
     else
       conn
     end
@@ -15,13 +13,18 @@ defmodule LiveMarkdownWeb.PlugRedirectTrailingSlash do
   def call(%{request_path: request_path} = conn, :with) when request_path != "/" do
     if not String.ends_with?(request_path, "/") do
       conn
-      |> Plug.Conn.put_status(301)
-      |> Phoenix.Controller.redirect(to: request_path <> "/")
-      |> Plug.Conn.halt()
+      |> redirect(request_path <> "/")
     else
       conn
     end
   end
 
   def call(conn, _), do: conn
+
+  defp redirect(conn, path) do
+    conn
+    |> Plug.Conn.put_status(301)
+    |> Phoenix.Controller.redirect(to: path)
+    |> Plug.Conn.halt()
+  end
 end
