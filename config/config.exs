1d3b9a8539de5fd20ffbdef68a6420199e741d21
Cody Brunner <me@codybrunner.dev>
2021-10-18 11:45:13 -0500

    fix(config): adds missing comma

--- config/config.exs
+++ config/config.exs
@@ -19,7 +19,7 @@ config :pardall_markdown, PardallMarkdown.Content,
   notify_content_reloaded: fn -> :ok end,
   is_markdown_metadata_required: true,
   is_content_draft_by_default: true,
-  metadata_parser: PardallMarkdown.MetadataParser.ElixirMap
+  metadata_parser: PardallMarkdown.MetadataParser.ElixirMap,
   remote_repository_url: "https://github.com/<username>/<project-name>git"
 
 config :pardall_markdown, PardallMarkdown.MetadataParser.JoplinNote,
