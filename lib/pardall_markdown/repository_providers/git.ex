b9679c2c24389512e1cfde5ebef38a6f4eeedd4b
Alfred Reinold Baudisch <alfred@alfredbaudisch.com>
2021-10-24 09:56:44 +0200

    Clone the repository inside PardallMarkdown's :root_path, since this is the path which FileWatcher watches, the idea is to keep the content logic in a single path as consistency; removed unused functions and callbacks; do not crash in case of existing repository, do not try to clone if the repo exists; crash in case the local cloned repository has local changes

--- lib/pardall_markdown/repository_providers/git.ex
+++ lib/pardall_markdown/repository_providers/git.ex
@@ -1,6 +1,6 @@
 defmodule PardallMarkdown.RepositoryProviders.Git do
   @moduledoc """
-  This implementation closely follows the implementation found in:
+  This implementation is based on the implementation found in:
   https://github.com/meddle0x53/blogit/blob/master/lib/blogit/settings.ex
   Many thanks to @meddle0x53 for this!
   """
@@ -11,19 +11,23 @@ defmodule PardallMarkdown.RepositoryProviders.Git do
   @repository_url Application.get_env(:pardall_markdown, PardallMarkdown.Content)[
     :remote_repository_url
   ]
-  @local_path @repository_url
-                |> String.split("/")
-                |> List.last()
-                |> String.trim_trailing(".git")
+  @local_path PardallMarkdown.Content.Utils.root_path()
 
   # Callbacks
-
+  @impl true
   def repository do
     repo = git_repository()
 
     case Git.pull(repo) do
       {:ok, msg} ->
         Logger.info("Pulling from git repository #{msg}")
+
+      # Crash in case of unstaged changes
+      {_, %Git.Error{
+        args: [], code: 128, command: "pull",
+        message: "error: cannot pull with rebase: You have unstaged changes.\nerror: please commit or stash them.\n" = message
+      }} ->
+        raise message
 
       {_, error} ->
         Logger.error("Error while pulling from git repository #{inspect(error)}")
@@ -32,6 +36,7 @@ defmodule PardallMarkdown.RepositoryProviders.Git do
     repo
   end
 
+  @impl true
   def fetch(repo) do
     Logger.info("Fetching data from #{@repository_url}")
 
@@ -56,84 +61,23 @@ defmodule PardallMarkdown.RepositoryProviders.Git do
     end
   end
 
+  @impl true
   def local_path, do: @local_path
 
-  def list_files(folder \\ Settings.posts_folder()) do
-    path = Path.join(@local_path, folder)
-    size = byte_size(path) + 1
-
-    path
-    |> recursive_ls()
-    |> Enum.map(fn <<_::binary-size(size), rest::binary>> -> rest end)
-  end
-
-  def file_in?(file), do: File.exists?(Path.join(@local_path, file))
-
-  def file_info(repository, file_path) do
-    %{
-      author: file_author(repository, file_path),
-      created_at: file_created_at(repository, file_path),
-      updated_at: file_updated_at(repository, file_path)
-    }
-  end
-
-  def read_file(file_path, folder \\ "") do
-    local_path() |> Path.join(folder) |> Path.join(file_path) |> File.read()
-  end
-
   # Private
-
-  defp log(repository, args), do: Git.log!(repository, args)
-
-  defp first_in_log(repository, args) do
-    repository
-    |> log(args)
-    |> String.split("\n")
-    |> List.first()
-    |> String.trim()
-  end
-
-  defp recursive_ls(path) do
-    cond do
-      File.regular?(path) ->
-        [path]
-
-      File.dir?(path) ->
-        path
-        |> File.ls!()
-        |> Enum.map(&Path.join(path, &1))
-        |> Enum.map(&recursive_ls/1)
-        |> Enum.concat()
-
-      true ->
-        []
-    end
-  end
-
   defp git_repository do
-    Logger.info("Cloning repository #{@repository_url}")
-
-    case Git.clone(@repository_url) do
-      {:ok, repo} -> repo
-      {:error, Git.Error} -> Git.new(@local_path)
+    if repository_exists?() do
+      Git.new(@local_path)
+    else
+      case Git.clone([@repository_url, @local_path]) do
+        {:ok, repo} ->
+          Logger.info("Cloning repository #{@repository_url}, into #{@local_path}")
+          repo
+        {:error, %Git.Error{}} ->
+          Git.new(@local_path)
+      end
     end
   end
 
-  defp file_author(repository, file_name) do
-    first_in_log(repository, ["--reverse", "--format=%an", file_name])
-  end
-
-  defp file_created_at(repository, file_name) do
-    case first_in_log(repository, ["--reverse", "--format=%ci", file_name]) do
-      "" -> DateTime.to_iso8601(DateTime.utc_now())
-      created_at -> created_at
-    end
-  end
-
-  defp file_updated_at(repository, file_name) do
-    case repository |> log(["-1", "--format=%ci", file_name]) |> String.trim() do
-      "" -> DateTime.to_iso8601(DateTime.utc_now())
-      updated_at -> updated_at
-    end
-  end
+  defp repository_exists?, do: File.exists?(Path.join(@local_path, ".git"))
 end
